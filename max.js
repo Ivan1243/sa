d383a03c518194c529c12c53ea3b77834a85d856
Ivan <ostankov-ii@mail.ru>
2020-06-19 11:38:00 +0300

    first commit

---
+++ max.js
@@ -0,0 +1,39 @@
+// берём Express
+var express = require('express');
+
+// создаём Express-приложение
+var app = express();
+
+// создаём маршрут для главной страницы
+// http://localhost:8080/
+app.use(express.static('img'));
+app.get('/', function(req, res) {
+  res.send('Hello');
+});
+
+app.get('/abc', function(req, res) {
+  res.send("<script>" + 
+           "function clicked(){"  +
+           "alert(document.getElementsByName('abc')[0].tagName)"   +
+             "}"+
+           "</script>" + 
+      "<style type='text/css'>" +
+        ".abc {" + 
+            "color:green;" + 
+        "}" + 
+        "</style>" + 
+           "<table name ='abc'>" +
+           "<tr name='a' bgcolor='blue' onclick='clicked()'>" +
+           "<td class='abc'>abc</td>" +
+           "<td type='button'>123</td>" +
+           "</tr>" +
+           "<tr>" +
+           "<td>456</td>" +
+           "<td ><img src='image.jpg'></td>" +
+           "</tr>" +
+           "</table>");
+});
+// запускаем сервер на порту 8080
+app.listen(8080);
+// отправляем сообщение
+console.log('Сервер стартовал!');
